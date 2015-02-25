579d445dca00fdc0a44aef829c9279d7d8a22605
Matthew Terrill <meterrill@gmail.com>
2015-02-25 14:39:18 -0800

    add ability to add list of links

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -2,7 +2,7 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.allLinks = [];
   factory.addLink = function() {
-    factory.allLinks.push({ title: factory.linkTitle, url: factory.linkUrl, factory.allLinks.length + 1, comments: []});
+    factory.allLinks.push({ title: factory.linkTitle, url: factory.linkUrl, id: factory.allLinks.length + 1, comments: []});
     factory.linkTitle = null;
     factory.linkUrl = null;
   };

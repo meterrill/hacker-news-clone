6c88003357eb346bdca9f0499da17c8b413c3070
Matthew Terrill <meterrill@gmail.com>
2015-02-25 15:43:44 -0800

    add vote feature

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -2,9 +2,24 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.allLinks = [];
   factory.addLink = function() {
-    factory.allLinks.push({ title: factory.linkTitle, url: factory.linkUrl, id: factory.allLinks.length + 1, comments: []});
+    factory.allLinks.push({ title: factory.linkTitle,
+                            url: factory.linkUrl,
+                            voteCounter: 0,
+                            id: factory.allLinks.length + 1,
+                            comments: [],
+                            upvote: function() {
+                              this.voteCounter += 1;
+                              }
+                            });
+    // factory.voteCounter = 0;
     factory.linkTitle = null;
     factory.linkUrl = null;
   };
+
+  // factory.upvote = function() {
+  //     factory.voteCounter += 1;
+  // };
+
   return factory;
+
 });

50107e82e4ed22b863c464720d72679d8d92bd26
Matthew Terrill <meterrill@gmail.com>
2015-02-25 11:57:19 -0800

    initial files

---
+++ app.js
@@ -0,0 +1,14 @@
+var hackerNews = angular.module('hackerNews', ['ui.router']);
+
+hackerNews.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('newLink', {
+    url: "/newLink",
+    templateUrl: "partials/newLink.html"
+    controller: 'LinksCtrl'
+  });
+});

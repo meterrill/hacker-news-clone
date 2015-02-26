e1be2e0e30f647b5918de6c8bc5a66a74a158bc1
Matthew Terrill <meterrill@gmail.com>
2015-02-26 10:22:19 -0800

    add rankingCounter

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -5,32 +5,26 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
     factory.allLinks.push({ title: factory.linkTitle,
                             url: factory.linkUrl,
                             voteCounter: 0,
+                            rankingCounter: 0,
                             id: factory.allLinks.length + 1,
-                            createdDate: function() {
-                              Date.now();
-                            },
                             comments: [],
                             upvote: function() {
                               this.voteCounter += 1;
+                              this.rankingCounter += 1;
                             }
                             });
     factory.linkTitle = null;
     factory.linkUrl = null;
   };
 
+  factory.mostRecent = function() {
+    factory.allLinks.forEach(function(link) {
+      if ((factory.allLinks.length - link.id) >= 2) {
+        link.rankingCounter -= 1;
+      }
+    });
+  };
+
   return factory;
 
 });
-
-
-// var count = 10;
-// var counterIncrement=-1;
-// var counter = setInterval(timer, 500);
-//
-// function timer() {
-//   count = count+counterIncrement;
-//   if (count == 0 || count == 10 ) {
-//         counterIncrement = -counterIncrement;
-//     }
-//   console.log(count);
-// }

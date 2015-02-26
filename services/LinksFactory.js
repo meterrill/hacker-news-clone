351dedd7733503e14050a686dc074ee4176869f5
Matthew Terrill <meterrill@gmail.com>
2015-02-25 16:45:33 -0800

    began timer feature

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -6,10 +6,13 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
                             url: factory.linkUrl,
                             voteCounter: 0,
                             id: factory.allLinks.length + 1,
+                            createdDate: function() {
+                              Date.now();
+                            },
                             comments: [],
                             upvote: function() {
                               this.voteCounter += 1;
-                              }
+                            }
                             });
     factory.linkTitle = null;
     factory.linkUrl = null;
@@ -18,3 +21,16 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
   return factory;
 
 });
+
+
+// var count = 10;
+// var counterIncrement=-1;
+// var counter = setInterval(timer, 500);
+//
+// function timer() {
+//   count = count+counterIncrement;
+//   if (count == 0 || count == 10 ) {
+//         counterIncrement = -counterIncrement;
+//     }
+//   console.log(count);
+// }
